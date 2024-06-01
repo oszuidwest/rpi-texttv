09a2a7d07f1efccc2040456c4732e5cf649346d2
rmens <r.mens@me.com>
2024-06-01 14:23:27 +0200

    Prod

---
+++ README.md
@@ -0,0 +1,46 @@
+# Raspberry Pi Text TV Setup
+
+This repository provides a script to configure a Raspberry Pi as a narrowcasting screen, displaying a webpage in full-screen mode using Chromium. The setup process includes installing essential packages, configuring the window manager, setting a fallback wallpaper, and enabling VNC for remote access.
+
+## Compatibilty 
+This setup is designed for Raspberry Pi 4 or newer models and is compatible only with Raspberry Pi OS Bookworm (64-bit) Lite. There's no need to install a full desktop environment, as this script installs and configures a lightweight alternative.
+
+## Usage
+To get started, install Raspberry Pi OS Bookworm (64-bit) and log in as a non-privileged user. It's important to avoid using `su` or `sudo` for root access during this process. Run the following command to execute the setup script:
+
+   ```bash
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/oszuidwest/rpi-teksttv/main/install.sh)".
+   ```
+
+This command will download and run the script, automatically installing the necessary packages and configuring your Raspberry Pi for kiosk mode.
+
+## Customization
+
+You can easily customize the script to use a different wallpaper or webpage by editing the `WALLPAPER_URL` and `CHROME_URL` variables at the beginning of the install.sh script:
+
+```bash
+WALLPAPER_URL="your_custom_wallpaper_url"
+CHROME_URL="your_custom_chrome_url"
+```
+
+## Architectural Overview
+
+- **X11**: The display server that provides the graphical environment necessary for running applications like Chromium on the Raspberry Pi. The script ensures X11 starts automatically on boot.
+- **Unclutter**: A utility to hide the mouse cursor when idle, for a clean screen presentation.
+- **Openbox**: A lightweight window manager that automatically starts and manages Chromium and other display settings, ensuring a minimal graphical environment.
+- **Feh**: Used to display a fallback wallpaper in the absence of other graphical content.
+- **Chromium**: Used to display the web application in full-screen kiosk mode, concealing all browser controls and unnecessary features such as translations.
+- **RealVNC**: Provides remote desktop access to the Raspberry Pi on the default port 5900.
+
+### Foreced resolution
+To ensure the Raspberry Pi displays the content correctly on different screens, the script includes settings to force a specific screen resolution. This is particularly useful when connecting to displays that may not automatically configure to the desired resolution.
+
+By default, the script configures the Raspberry Pi to use a standard HD resolution (1920x1080) at 60Hz. If you need a diffrent resolution, you can change the `VIDEO_OPTION` variable and the `xrandr` commands. By default only the main screen (HDMI-A) is used.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Contributions are welcome! Feel free to submit issues and pull requests for improvements and additional features.
---
+++ install.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+
+# Variables (Update these if needed)
+WALLPAPER_URL="https://raw.githubusercontent.com/oszuidwest/windows10-baseline/main/assets/ZWTV-wallpaper.png"
+CHROME_URL="https://teksttv.zuidwesttv.nl/"
+FUNCTIONS_LIB_URL="https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh"
+
+# Constants
+FUNCTIONS_LIB_PATH="/tmp/functions.sh"
+CMDLINE_FILE="/boot/firmware/cmdline.txt"
+VIDEO_OPTION="video=HDMI-A-1:1920x1080@60D"
+
+# Remove old functions library and download the latest version
+rm -f "$FUNCTIONS_LIB_PATH"
+if ! curl -s -o "$FUNCTIONS_LIB_PATH" "$FUNCTIONS_LIB_URL"; then
+  echo -e "*** Failed to download functions library. Please check your network connection! ***"
+  exit 1
+fi
+
+# Source the functions file
+# shellcheck source=/dev/null
+source "$FUNCTIONS_LIB_PATH"
+
+# Set color variables
+set_colors
+
+# Check if the script is running as root
+check_user_privileges normal
+
+# Ensure the script is running on a supported platform (Linux, 64-bit, Raspberry Pi 4 or newer)
+is_this_linux
+is_this_os_64bit
+check_rpi_model 4
+
+# Clear the terminal for a clean start
+clear
+
+# Display Banner
+cat << "EOF"
+ ______   _ ___ ______        _______ ____ _____   _______     __
+|__  / | | |_ _|  _ \ \      / / ____/ ___|_   _| |_   _\ \   / /
+  / /| | | || || | | \ \ /\ / /|  _| \___ \ | |     | |  \ \ / / 
+ / /_| |_| || || |_| |\ V  V / | |___ ___) || |     | |   \ V /  
+/____|\___/|___|____/  \_/\_/  |_____|____/ |_|     |_|    \_/   
+EOF
+
+# Greet the user
+echo -e "${GREEN}⎎ Raspberry Pi Tekst TV Set-up${NC}\n\n"
+ask_user "DO_UPDATES" "y" "Do you want to perform all OS updates? (y/n)" "y/n"
+
+# Set the system timezone
+set_timezone Europe/Amsterdam
+
+# Configure video resolution if not already set
+echo -e "${BLUE}►► Setting video resolution...${NC}"
+if ! grep -q "$VIDEO_OPTION" "$CMDLINE_FILE"; then
+  echo "No resolution force option found in $CMDLINE_FILE, appending..."
+  sudo sed -i "$ s/$/ $VIDEO_OPTION/" "$CMDLINE_FILE"
+  echo "Resolution set to $VIDEO_OPTION"
+else
+  echo "Resolution $VIDEO_OPTION already exists in $CMDLINE_FILE, no changes made."
+fi
+
+# Perform OS updates if requested by the user
+if [ "$DO_UPDATES" == "y" ]; then
+  update_os silent
+fi
+
+# Install necessary packages
+install_packages silent xserver-xorg x11-xserver-utils x11-utils xinit openbox unclutter chromium-browser feh ttf-mscorefonts-installer fonts-crosextra-carlito fonts-crosextra-caladea realvnc-vnc-server
+
+# Set up the fallback wallpaper
+sudo mkdir -p /var/fallback
+sudo wget -q "$WALLPAPER_URL" -O /var/fallback/fallback.png
+
+# Configure Openbox
+echo -e "${BLUE}►► Configuring Openbox...${NC}"
+mkdir -p ~/.config/openbox
+cat << EOF > ~/.config/openbox/autostart
+#!/bin/bash
+xset -dpms          # Disable DPMS (Energy Star) features.
+xset s off          # Disable screen saver.
+xset s noblank      # Don't blank the video device.
+
+# Set the resolution and position monitor 2 to the right of monitor 1
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode HDMI-1 1920x1080_60.00
+
+# Set HDMI-1 as the primary monitor
+xrandr --output HDMI-1 --primary --mode 1920x1080_60.00 --rate 60
+
+# Hide the mouse cursor when idle
+unclutter -idle 0 &
+
+# Display the fallback image as a background using feh
+feh --fullscreen /var/fallback/fallback.png &
+
+# Wait for feh to start
+sleep 2
+
+# Start Chromium in kiosk mode
+chromium-browser --kiosk --noerrdialogs --disable-infobars --disable-session-crashed-bubble \
+    --disable-features=TranslateUI --app=$CHROME_URL --incognito \
+    --disable-extensions --disable-background-networking --disable-background-timer-throttling \
+    --disable-client-side-phishing-detection --disable-default-apps --disable-hang-monitor \
+    --disable-popup-blocking --disable-prompt-on-repost --disable-sync --metrics-recording-only \
+    --no-first-run --no-default-browser-check --disable-component-update \
+    --disable-backgrounding-occluded-windows --disable-renderer-backgrounding
+EOF
+
+chmod +x ~/.config/openbox/autostart
+
+# Configure X11 and VNC to start on boot
+echo -e "${BLUE}►► Enabling X11 and VNC on boot...${NC}"
+cat << 'EOF' >> ~/.profile
+if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
+  startx
+fi
+EOF
+
+sudo raspi-config nonint do_boot_behaviour B2
+sudo raspi-config nonint do_vnc 0
+
+# Clean up unnecessary packages
+echo -e "${BLUE}►► Cleaning up unnecessary packages...${NC}"
+sudo apt autoremove -y
+
+# Reboot the system
+echo -e "${GREEN}Configuration complete. The system will now reboot.${NC}\n\n"
+sudo reboot
